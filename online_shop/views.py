92e1cb58568ba7174deee75b01ddaaa45a88afb4
Shohnur <Shohnur@gamil.com>
2024-08-06 16:19:48 +0500

    adding filter and search

--- online_shop/views.py
+++ online_shop/views.py
@@ -2,23 +2,44 @@ from typing import Optional
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from online_shop.forms import CommentModelForm, OrderModelForm, ProductModelForm
 from online_shop.models import Category, Product, Comment
-
-
-# Create your views here.
+from django.db.models import Q
 
 
 def product_list(request, category_id: Optional[int] = None):
     categories = Category.objects.all().order_by('id')
+    search = request.GET.get('q')
+    filter_type = request.GET.get('filter', '')
     if category_id:
+        if filter_type == 'expensive':
+            products = Product.objects.filter(category=category_id).order_by('-price')
+        elif filter_type == 'cheap':
+            products = Product.objects.filter(category=category_id).order_by('price')
+        elif filter_type == 'rating':
+            products = Product.objects.filter(Q(category=category_id) & Q(rating__gte=4)).order_by('-rating')
 
-        products = Product.objects.filter(category=category_id)
+        else:
+            products = Product.objects.filter(category=category_id)
 
     else:
-        products = Product.objects.all()
+        if filter_type == 'expensive':
+            products = Product.objects.all().order_by('-price')
+        elif filter_type == 'cheap':
+            products = Product.objects.all().order_by('price')
+        elif filter_type == 'rating':
+            products = Product.objects.filter(Q(rating__gte=4)).order_by('-rating')
+            print(products)
+
+        else:
+            products = Product.objects.all()
+
+    if search:
+        products = products.filter(Q(name__icontains=search) | Q(comments__name__icontains=search))
+
     context = {
         'products': products,
         'categories': categories
@@ -27,29 +48,17 @@ def product_list(request, category_id: Optional[int] = None):
 
 
 def product_detail(request, product_id):
+    categories = Category.objects.all()
     product = Product.objects.get(id=product_id)
     comments = Comment.objects.filter(product=product_id, is_provide=True).order_by('-id')
     context = {
         'product': product,
-        'comments': comments
+        'comments': comments,
+        'categories': categories
     }
 
     return render(request, 'online_shop/detail.html', context)
 
-
-# def add_comment(request, product_id):
-#     product = get_object_or_404(Product, id=product_id)
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         body = request.POST.get('body')
-#         comment = Comment(name=name, email=email, body=body)
-#         comment.product = product
-#         comment.save()
-#         return redirect('product_detail', product_id)
-#     else:
-#         pass
-#     return render(request, 'online_shop/detail.html')
 
 def add_comment(request, product_id):
     product = get_object_or_404(Product, id=product_id)
